f781f933ef2506b20fe5610f0f67abfb7c0359c5
JeongyeopHan <133887543+JeongyeopHan@users.noreply.github.com>
2024-05-31 06:24:59 +0900

    Update task1_download.py

--- task1_download.py
+++ task1_download.py
@@ -1,14 +1,8 @@
 import os
 from sec_edgar_downloader import Downloader
 
-def download_filings(ticker, download_dir="."):
-    # Initialize Downloader
-    dl = Downloader("Jeong", "20150613rke3@gmail.com", download_dir)
-
-    # Download all 10-K filings for the ticker from 2023 onward
+def download_filings(ticker):
+    dl = Downloader("Jeong", "20150613rke3@gmail.com", ".")
     dl.get("10-K", ticker, after="1994-12-31", before="2024-01-01")
-
-    # Directory where filings are downloaded
-    download_dir = os.path.join(download_dir, "sec-edgar-filings", ticker, "10-K")
-
+    download_dir = os.path.join(".", "sec-edgar-filings", ticker, "10-K")
     return download_dir
