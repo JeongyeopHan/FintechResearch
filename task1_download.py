49e643dedccf95a0ae0c21503f4c43196b2bed5b
JeongyeopHan <133887543+JeongyeopHan@users.noreply.github.com>
2024-05-31 05:58:17 +0900

    Update task1_download.py

--- task1_download.py
+++ task1_download.py
@@ -1,15 +1,11 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import os
 from sec_edgar_downloader import Downloader
 
-def download_10k_filings(ticker, start_year=1995, end_year=2023):
-    dl = Downloader("Jeong", "20150613rke3@gmail.com", ".")
+def download_10k_filings(ticker, start_year=1994, end_year=2023):
+    dl = Downloader("Jeong", "20150613rke3@gmail.com", '.')
     for year in range(start_year, end_year + 1):
         try:
-            dl.get("10-K", ticker, after="2018-11-01", before="2023-12-31")
+            dl.get("10-K", ticker, after=f"{year}-01-01", before=f"{year}-12-31")
         except Exception as e:
             print(f"Failed to download 10-K for {ticker} for year {year}: {e}")
 
